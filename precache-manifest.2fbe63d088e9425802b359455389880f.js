2a0cd0eb9fe262b663e388e8780fced8c5038165
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2020-08-02 16:23:11 -0600

    Updates

---
+++ precache-manifest.2fbe63d088e9425802b359455389880f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882c4d4e415be10329bf3ff382b0e714",
+    "url": "/simple-forge-github.io/index.html"
+  },
+  {
+    "revision": "fe36685bc99fd307debb",
+    "url": "/simple-forge-github.io/static/css/main.670f6eb9.chunk.css"
+  },
+  {
+    "revision": "ce9f212d1dea41ca16d4",
+    "url": "/simple-forge-github.io/static/js/2.df811083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-forge-github.io/static/js/2.df811083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe36685bc99fd307debb",
+    "url": "/simple-forge-github.io/static/js/main.735021d7.chunk.js"
+  },
+  {
+    "revision": "8d007b00cade3695c684",
+    "url": "/simple-forge-github.io/static/js/runtime-main.f00a6c8f.js"
+  }
+]);
